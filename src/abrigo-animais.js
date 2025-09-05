469b1735559eb2f8d874c61ef9a10d2baef89130
Murilo Silveira <murilorms22@gmail.com>
2025-09-05 17:53:28 -0300

    definições iniciais

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,22 @@
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    const listaAnimais = {
+      'Rex': { tipo: 'cão', brinquedos: ['RATO, BOLA']},
+      'Mimi': { tipo: 'gato', brinquedos: ['BOLA, LASER']},
+      'Fofo': { tipo: 'gato', brinquedos: ['BOLA, RATO, LASER']},
+      'Zero': { tipo: 'gato', brinquedos: ['RATO, BOLA']},
+      'Bola': { tipo: 'cão', brinquedos: ['CAIXA, NOVELO']},
+      'Bebe': { tipo: 'cão', brinquedos: ['LASER, RATO, BOLA']},
+      'Loco': { tipo: 'jabuti', brinquedos: ['SKATE, RATO']},
+    }
+
+    const pessoa1 = brinquedosPessoa1.split(',');
+    const pessoa2 = brinquedosPessoa2.split(',');
+    const animais = ordemAnimais.split(',');
+
+    
+
   }
 }
 
