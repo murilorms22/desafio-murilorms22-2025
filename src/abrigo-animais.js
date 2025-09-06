d3094b0379754d1f55b77016d031e7294e3c9678
Murilo Silveira <murilorms22@gmail.com>
2025-09-06 10:59:18 -0300

    features: laços com for para validação de dados passados como parâmetro

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -11,11 +11,56 @@ class AbrigoAnimais {
       'Loco': { tipo: 'jabuti', brinquedos: ['SKATE, RATO']},
     }
 
-    const pessoa1 = brinquedosPessoa1.split(',');
-    const pessoa2 = brinquedosPessoa2.split(',');
+    const brinqPessoa1 = brinquedosPessoa1.split(',');
+    const brinqPessoa2 = brinquedosPessoa2.split(',');
     const animais = ordemAnimais.split(',');
 
-    
+    let animaisPessoa1 = [];
+    let animaisPessoa2 = [];
+    let resultadoFinal = [];
+
+    //verificando se o animal é válido
+    let animaisVistos = [];
+    for (let i = 0; i < animais.length; i++) {
+      let animal = animais[i];
+      if(!listaAnimais[animal] || animaisVistos.includes(animal)) {
+        return { erro: "Animal inváldo." }
+      }
+      animaisVistos.push(animal);
+    }
+
+    //verificando se os brinquedos são válidos
+    let brinquedosVistosP1 = [];
+    for(let i = 0; i < brinqPessoa1.length; i++) {
+      let brinquedo = brinqPessoa1[i];
+      if(brinquedosVistosP1.includes(brinquedo)){
+        return { erro: 'Brinquedo duplicado.' }
+      }
+      brinquedosVistosP1.push(brinquedo);
+    }
+
+    let brinquedosVistosP2 = [];
+    for(let i = 0; i < brinqPessoa2.length; i++) {
+      let brinquedo = brinqPessoa2[i];
+      if(brinquedosVistosP2.includes(brinquedo)){
+        return { erro: 'Brinquedo duplicado.' }
+      }
+      brinquedosVistosP2.push(brinquedo);
+    }
+
+
+
+    if(animaisPessoa1.length > 3) {
+      return { erro: 'Excedeu o limite de animais.' };
+    } else {
+
+    }
+
+    if(animaisPessoa2.length > 3) {
+      return { erro: 'Excedeu o limite de animais.' };
+    }
+
+
 
   }
 }
